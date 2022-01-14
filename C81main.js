f9920ab7bf2d39f3443736be30b4732116bbee37
adrigoswame <91961263+adrigoswame@users.noreply.github.com>
2022-01-14 20:20:57 +0530

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,93 @@
+canvas=document.getElementById("myCanvas");
+ctx=canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.strokeStyle= "grey";
+ctx.linewidth=4;
+ctx.rect(150, 143, 430, 350, );
+ctx.stroke();
+
+
+ctx.beginPath();
+ctx.strokeStyle= "blue";
+ctx.lineWidth=5;
+ctx.arc(250, 210, 50, 0, 2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle= "black";
+ctx.lineWidth=5;
+ctx.arc(370, 210, 50, 0, 2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle= "red";
+ctx.lineWidth=5;
+ctx.arc(490, 210, 50, 0, 2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle= "yellow";
+ctx.lineWidth=5;
+ctx.arc(310, 280, 50, 0, 2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle= "green";
+ctx.lineWidth=5;
+ctx.arc(430s, 280, 50, 0, 2*Math.PI);
+ctx.stroke();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
